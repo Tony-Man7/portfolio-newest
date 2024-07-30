5aed4c3224fbdbee0fd90e96a210d957aff4866d
Tony-Man7 <antMan.dev.07@gmail.com>
2024-07-30 20:34:54 +0800

    Update project with new images and fixed paths

--- src/components/homepage/Services.jsx
+++ src/components/homepage/Services.jsx
@@ -1,39 +1,36 @@
 import ServiceUi from "../ui/ServiceUi";
 import Heading from "../ui/Heading";
+
 import illustration from "../../assets/images/illustration.png";
+import htmlImage from "../../assets/images/html.png";
+import cssImage from "../../assets/images/css.png";
+import javascriptImage from "../../assets/images/javascript.png";
+import phpImage from "../../assets/images/php.png";
+import tailwindcssImage from "../../assets/images/tailwindcss.png";
+import bootstrapImage from "../../assets/images/bootstrap.png";
+import reactjsImage from "../../assets/images/reactjs.png";
+import nodejsImage from "../../assets/images/nodejs.png";
+import expressImage from "../../assets/images/express.png";
+import mongodbImage from "../../assets/images/mongodb.png";
 
 export default function Services() {
-  // const expertiseItems = [
-  //   "Web Design",
-  //   "UI/UX Design",
-  //   "Branding",
-  //];
-
   const toolBoxItems = [
-    { name: "HTML", image: "src/assets/images/html.png" },
-    { name: "CSS", image: "src/assets/images/css.png" },
-    { name: "JavaScript", image: "src/assets/images/javascript.png" },
-    { name: "PHP", image: "src/assets/images/php.png" },
-    { name: "TailwindCSS", image: "src/assets/images/tailwindcss.png" },
-    { name: "Bootsraps", image: "src/assets/images/bootstrap.png" },
-    { name: "ReactJS", image: "src/assets/images/reactjs.png" },
-    { name: "NodeJS", image: "src/assets/images/nodejs.png" },
-    { name: "ExpressJS", image: "src/assets/images/express.png" },
-    { name: "MongoDB", image: "src/assets/images/mongodb.png" }
+    { name: "HTML", image: htmlImage },
+    { name: "CSS", image: cssImage },
+    { name: "JavaScript", image: javascriptImage },
+    { name: "PHP", image: phpImage },
+    { name: "TailwindCSS", image: tailwindcssImage },
+    { name: "Bootstrap", image: bootstrapImage },
+    { name: "ReactJS", image: reactjsImage },
+    { name: "NodeJS", image: nodejsImage },
+    { name: "ExpressJS", image: expressImage },
+    { name: "MongoDB", image: mongodbImage }
   ];
-  
+
   return (
     <section id="services" className="my-[10%]" aria-label="services">
       <Heading title="services" />
       <div className="space-y-14">
-        {/* <ServiceUi
-          title="My expertises."
-          description="I focus on all things design and web related. With each of my
-          services, my goal is to deliver an impactful and elevating
-          digital experience for everyone."
-          items={expertiseItems}
-        /> */}
-
         <ServiceUi
           title="My Tech Stack."
           items={toolBoxItems}
